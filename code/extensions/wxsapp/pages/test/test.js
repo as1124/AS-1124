5b6bc99f2bd9dd5cc4ad9d5577b7319e04dd7958
huangjw <huangjw@primeton.com>
2018-08-10 15:57:17 +0800

    update: test sample for htmlx2wxsapp

---
+++ code/extensions/wxsapp/pages/test/test.js
@@ -0,0 +1,78 @@
+// pages/test.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    date:"2018-09-01"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  testclick: function(event){
+    wx.showToast({
+      title: '测试点击事件',
+      duration: 1500
+    })
+  },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value);
+    this.setData({
+      date: e.detail.value
+    })
+  }
+});
