81e2dcb4256c73ac715dd996c42c5201fedd3213
As-1124 <as1124huang@gmail.com>
2018-12-27 17:53:08 +0800

    update: learn for android topic Material Design

--- code/Android/andriod_advance/ch2_material_design/src/main/java/com/as1124/ch2_material/MainActivity.java
+++ code/Android/andriod_advance/ch2_material_design/src/main/java/com/as1124/ch2_material/MainActivity.java
@@ -1,13 +1,35 @@
 package com.as1124.ch2_material;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import com.as1124.ch2_material.textinput.LoginActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.but_snackbar).setOnClickListener(v -> snackbarUsage());
+        findViewById(R.id.but_to_login).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
+
+    }
+
+    private void snackbarUsage() {
+        View frameLayout = getWindow().getDecorView().findViewById(android.R.id.content);
+        View userRoot = ((ViewGroup) frameLayout).getChildAt(0);
+        Snackbar snackbar = Snackbar.make(userRoot, "来呀, 快活呀", Snackbar.LENGTH_INDEFINITE);
+        // 只能显示一个Action
+        snackbar.setAction("Fuck one", v -> Toast.makeText(MainActivity.this, "游刃有余", Toast.LENGTH_SHORT).show())
+                .setAction("Three Happy?", v -> Toast.makeText(this, "神仙也嫉妒", Toast.LENGTH_SHORT).show())
+                .setAction("As much as possible?", v -> Toast.makeText(this, "有预谋的谋杀", Toast.LENGTH_SHORT).show())
+                .show();
     }
 }
---
+++ code/Android/andriod_advance/ch2_material_design/src/main/java/com/as1124/ch2_material/textinput/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.as1124.ch2_material.textinput;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.as1124.ch2_material.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
