87f6b527c5ba64f5456b850dea03a05143185b7e
as-1124 <as1124huang@gmail.com>
2019-02-13 17:52:54 +0800

    ADD: Android advance skills for multi threads

---
+++ code/Android/andriod_advance/ch4_multi_thread/src/main/java/com/as1124/ch4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.as1124.ch4;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String file = getApplicationInfo().dataDir;
+        File dataDir = Environment.getDataDirectory();
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 11);
+            }
+        }
+    }
+}
